e422959fb8ffcb4612cf0fd523a8e0c8a5139039
fanyucai1 <fanyucai1@126.com>
2019-12-04 17:28:47 +0800

    no

--- TSO500_script/run_bcl.py
+++ TSO500_script/run_bcl.py
@@ -15,7 +15,7 @@ class Myconf(configparser.ConfigParser):
     def optionxform(self, optionstr):
         return optionstr
 
-def run(analysis,runFolder,SampleSheet,configfile,project_name):
+def run_docker(analysis,runFolder,SampleSheet,configfile):
      config = Myconf()
      config.read(configfile)
      TSO500=config.read('software','TSO500')
@@ -24,42 +24,46 @@ def run(analysis,runFolder,SampleSheet,configfile,project_name):
           cmd="cd %s && %s --resourcesFolder %s/resources --runFolder %s --analysisFolder %s --sampleSheet %s"%(os.path.dirname(TSO500),TSO500,os.path.dirname(TSO500),runFolder,analysis,SampleSheet)
           subprocess.check_call(cmd,shell=True)
           subprocess.check_call("echo done >%s/docker_run.log"%(analysis),shell=True)
-          outdir = analysis + "/final_result"
-          ID = []
-          if not os.path.exists(outdir):
-               os.makedirs(outdir)
-               os.makedirs("%s/CNV" % (outdir))
-               os.makedirs("%s/SNV_indel" % (outdir))
-               os.makedirs("%s/TMB_MSI" % (outdir))
-          for (root, dirs, files) in os.walk(analysis):
-               for file in files:
-                    tmp = os.path.join(root, file)
-                    sample = tmp.split("/")[-2]
-                    if tmp.endswith("CopyNumberVariants.vcf"):
-                         ID.append(sample)
-                         print(tmp)
-                         core.format_CNV.run(tmp, "%s/CNV" % (outdir), sample)
-                    elif tmp.endswith("TMB_Trace.tsv"):
-                         print(tmp)
-                         gvcf = tmp.replace("TMB_Trace.tsv", "MergedSmallVariants.genome.vcf")
-                         core.format_SNV_indel.run(tmp, gvcf, "%s/SNV_indel" % (outdir), sample)
-                         core.anno_vcf.run("%s/SNV_indel/%s.vcf" % (outdir, sample), "%s/SNV_indel" % (outdir), sample,
-                                           configfile)
-                         subprocess.check_call("rm -rf %s/SNV_indel/%s.vcf" % (outdir, sample), shell=True)
-                    elif tmp.split("/")[-3] == "StitchedRealigned" and tmp.endswith("bam"):
-                         print(tmp)
-                         core.copy_file.run(project_name, tmp)
-                         core.copy_file.run(project_name, "%s.bai" % (tmp))
-                    elif tmp.split("/")[-1] == "MetricsOutput.tsv":
-                         print(tmp)
-                         subprocess.check_call("cp %s -rf %s" % (tmp, outdir), shell=True)
-                    elif tmp.endswith("CombinedVariantOutput.tsv"):
-                         print(tmp)
-                         subprocess.check_call("cp %s -rf %s/TMB_MSI" % (tmp, outdir), shell=True)
-                    else:
-                         pass
-          subprocess.check_call("cd %s && tar -zcvf final_result.tar.gz final_result/" % (analysis), shell=True)
-          core.copy_file.run(project_name, "%s/final_result.tar.gz" % (analysis))
+
+def run(indir, project_name, configfile):
+  config = Myconf()
+  config.read(configfile)
+  outdir = indir + "/final_result"
+  ID = []
+  if not os.path.exists(outdir):
+      os.makedirs(outdir)
+      os.makedirs("%s/CNV" % (outdir))
+      os.makedirs("%s/SNV_indel" % (outdir))
+      os.makedirs("%s/TMB_MSI" % (outdir))
+  for (root, dirs, files) in os.walk(indir):
+      for file in files:
+          tmp = os.path.join(root, file)
+          sample = tmp.split("/")[-2]
+          if tmp.endswith("CopyNumberVariants.vcf"):
+              ID.append(sample)
+              print(tmp)
+              core.format_CNV.run(tmp, "%s/CNV" % (outdir), sample)
+          elif tmp.endswith("TMB_Trace.tsv"):
+              print(tmp)
+              gvcf = tmp.replace("TMB_Trace.tsv", "MergedSmallVariants.genome.vcf")
+              core.format_SNV_indel.run(tmp, gvcf, "%s/SNV_indel" % (outdir), sample)
+              core.anno_vcf.run("%s/SNV_indel/%s.vcf" % (outdir, sample), "%s/SNV_indel" % (outdir), sample,
+                                configfile)
+              subprocess.check_call("rm -rf %s/SNV_indel/%s.vcf" % (outdir, sample), shell=True)
+          elif tmp.split("/")[-3] == "StitchedRealigned" and tmp.endswith("bam"):
+              print(tmp)
+              core.copy_file.run(project_name, tmp)
+              core.copy_file.run(project_name, "%s.bai" % (tmp))
+          elif tmp.split("/")[-1] == "MetricsOutput.tsv":
+              print(tmp)
+              subprocess.check_call("cp %s -rf %s" % (tmp, outdir), shell=True)
+          elif tmp.endswith("CombinedVariantOutput.tsv"):
+              print(tmp)
+              subprocess.check_call("cp %s -rf %s/TMB_MSI" % (tmp, outdir), shell=True)
+          else:
+              pass
+  subprocess.check_call("cd %s && tar -zcvf final_result.tar.gz final_result/" % (indir), shell=True)
+  core.copy_file.run(project_name, "%s/final_result.tar.gz" % (indir))
 
 if __name__=="__main__":
     parer=argparse.ArgumentParser()
@@ -70,5 +74,5 @@ if __name__=="__main__":
     parer.add_argument("-p","--project",help="project name",required=True)
     parer.add_argument("-t","--type",help="fastq or bcl",required=True,choices=["bcl","fastq"])
     args=parer.parse_args()
-    if args.type=="bcl":
-         run(args.analysis, args.bcl, args.samplesheet, args.configfile, args.project)
+    run_docker(args.analysis, args.bcl, args.samplesheet, args.config)
+    run(args.analysis, args.project, args.config)
